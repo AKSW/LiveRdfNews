e0e9818c4d4d50373b856c96e5dfcd8eb7f1bbd9
Daniel Gerber <dgerber@informatik.uni-leipzig.de>
2012-08-09 11:47:35 +0200

    added deduplication interface

---
+++ rdflivenews-core/src/main/java/org/aksw/simba/rdflivenews/deduplication/Deduplication.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.aksw.simba.rdflivenews.deduplication;
+
+
+/**
+ * @author Daniel Gerber <dgerber@informatik.uni-leipzig.de>
+ *
+ */
+public interface Deduplication {
+
+}
